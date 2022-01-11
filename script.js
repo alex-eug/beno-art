b870446206825c3b5b8e0425c3a9b229e219b7af
Alexandre Eugene <you@example.com>
2022-01-11 15:39:34 +0100

    contact

--- script.js
+++ script.js
@@ -2,111 +2,117 @@ const oeuvres = [
     {
         id: 1,
         title: "Alpha mini",
-        img: "pictures/Alphamini1.jpeg",
+        img: "pictures/Alphamini1.jpg",
         alt: "..."
     },
     {
         id: 2,
         title: "Callisto",
-        img: "pictures/Callisto.jpeg",
+        img: "pictures/Callisto.jpg",
         alt: "...",
     },
     {
         id: 3,
         title: "Céphée",
-        img: "pictures/Céphée.jpeg",
+        img: "pictures/Céphée.jpg",
         alt: "...",
     },
     {
         id: 4,
         title: "Beta",
-        img: "pictures/Beta.jpeg",
+        img: "pictures/Beta.jpg",
         alt: "...",
     },
     {
         id: 5,
         title: "Céphée Maxi",
-        img: "pictures/CéphéeMaxi.jpeg",
+        img: "pictures/CéphéeMaxi.jpg",
         alt: "...",
     },
     {
         id: 6,
         title: "Deimos",
-        img: "pictures/Deimos.jpeg",
+        img: "pictures/Deimos.jpg",
         alt: "...",
     },
     {
         id: 7,
         title: "Delta",
-        img: "pictures/Delta.jpeg",
+        img: "pictures/Delta.jpg",
         alt: "...",
     },
     {
         id: 8,
         title: "Céphée",
-        img: "pictures/Céphée.jpeg",
+        img: "pictures/Céphée2.jpg",
         alt: "...",
     },
     {
         id: 9,
         title: "Cassiopée",
-        img: "pictures/Cassiopée.jpeg",
+        img: "pictures/Cassiopée.jpg",
         alt: "...",
     },
     {
         id: 10,
-        title: "Encelade",
-        img: "pictures/Europe.jpeg",
+        title: "Europe",
+        img: "pictures/Europe.jpg",
         alt: "...",
     },
     {
         id: 11,
         title: "Kardia",
-        img: "pictures/Kardia.jpeg",
+        img: "pictures/Kardia.jpg",
         alt: "...",
     },
     {
         id: 12,
         title: "Le cube",
-        img: "pictures/lecube.jpeg",
+        img: "pictures/lecube.jpg",
         alt: "...",
     },
     {
         id: 13,
-        title: "Kardia",
-        img: "pictures/Kardia.jpeg",
+        title: "Encelade",
+        img: "pictures/Encelade.jpg",
         alt: "...",
     },
     {
         id: 14,
         title: "Orion",
-        img: "pictures/ORION.jpeg",
+        img: "pictures/ORION.jpg",
         alt: "...",
     },
     {
         id: 15,
         title: "Titan suspension",
-        img: "pictures/titansuspension3.jpeg",
+        img: "pictures/titansuspension3.jpg",
         alt: "...",
     },
     {
         id: 16,
         title: "Titan lévitation",
-        img: "pictures/Titanlévitation.jpeg",
+        img: "pictures/Titanlévitation.jpg",
         alt: "...",
     },
     {
         id: 17,
         title: "Titan",
-        img: "pictures/Titan.jpeg",
+        img: "pictures/Titan.jpg",
         alt: "...",
     },
     {
         id: 18,
         title: "Véga",
-        img: "pictures/Véga.jpeg",
+        img: "pictures/Véga.jpg",
         alt: "...",
     },
+    // {
+    //     id: 19,
+    //     title: "Callisto terre",
+    //     img: "pictures/Callistoterrecouleur.jpg",
+    //     alt: "...",
+    // },
 
 ]
 
@@ -114,41 +120,83 @@ const gallery = [
     {
         id: 1,
         title: "....",
-        img: "....",
+        img: "gallerie/Hypérion-bleu.jpg",
         alt: "...",
     },
     {
         id: 2,
         title: "....",
-        img: "....",
+        img: "gallerie/armoire.jpg",
         alt: "...",
     },
     {
         id: 3,
         title: "....",
-        img: "....",
+        img: "gallerie/Hypérion-galerie-photo.jpg",
         alt: "...",
     },
     {
         id: 4,
         title: "....",
-        img: "....",
+        img: "gallerie/IMG_1426.jpg",
         alt: "...",
     },
     {
         id: 5,
         title: "....",
-        img: "....",
+        img: "gallerie/IMG_1427.jpg",
         alt: "...",
     },
     {
         id: 6,
         title: "....",
-        img: "....",
+        img: "gallerie/IMG_1430.jpg",
         alt: "...",
     },
+    // {
+    //     id: 7,
+    //     title: "....",
+    //     img: "gallerie/lampe-color.jpg",
+    //     alt: "...",
+    // },
+    {
+        id: 8,
+        title: "....",
+        img: "gallerie/lampe.jpg",
+        alt: "...",
+    },
+    {
+        id: 9,
+        title: "....",
+        img: "gallerie/chaos.jpg",
+        alt: "...",
+    },
+    {
+        id: 10,
+        title: "....",
+        img: "gallerie/beno-chaise.jpg",
+        alt: "...",
+    },
+    // {
+    //     id: 11,
+    //     title: "....",
+    //     img: "gallerie/IMG_1426.jpg",
+    //     alt: "...",
+    // },
 ]
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.nav-list');
+btnMenu.addEventListener('click',()=>{
+    menu.classList.toggle('active')
+})
 
+const allNavLinks = document.querySelectorAll('.item-nav');
+ allNavLinks.forEach(item=>{
+    item.addEventListener('click',()=>{
+
+        menu.classList.toggle('active')
+    }) 
+ })
 
 const containerAll = document.querySelector('.container-all')
 for (let i = 0; i < oeuvres.length; i++) {
@@ -162,10 +210,13 @@ for (let i = 0; i < oeuvres.length; i++) {
     containerAll.appendChild(divContainer)
     divContainer.appendChild(img)
     divContainer.appendChild(spann)
-    console.log(containerAll);
+   
 }
-// <div class="container">
-//   <img src="pictures/6AFE3D9C-E1E5-45C8-BB40-D8B7631D0A07.jpeg" alt="">
-//    <span class="title">Lorem ipsum dolor</span>
-//   <span class="text">Morbi diam viverra mattis sociis magna, habitasse penatibus non lectus</span>
-// </div>
+const galleryContainer = document.querySelector('.gallery');
+for(gall of gallery){
+    let divGallery = document.createElement("div")
+   let imgGallery= document.createElement('img');
+   imgGallery.setAttribute("src", gall.img)
+   divGallery.appendChild(imgGallery)
+    galleryContainer.appendChild (divGallery)
+}
